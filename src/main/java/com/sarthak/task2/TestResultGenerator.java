6be7b7724b6da7396b75ed7d0a78abfc71ec252a
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-28 19:18:12 +0530

    added the methods getDetailsOfFailedStudents() and sortStudentsForRanking()

--- src/main/java/com/sarthak/task2/TestResultGenerator.java
+++ src/main/java/com/sarthak/task2/TestResultGenerator.java
@@ -5,5 +5,15 @@
  */
 package com.sarthak.task2;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TestResultGenerator {
+    public HashSet<String> getDetailsOfFailedStudents(Set<Student> studentSet) {
+        return null;
+    }
+
+    public Set<Student> sortStudentsForRanking(HashSet<Student> studentSet) {
+        return null;
+    }
 }
