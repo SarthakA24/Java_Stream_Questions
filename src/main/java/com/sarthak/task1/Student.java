4e86a8cb3634d6e64dc49ade52337d774dc32e5e
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-28 17:45:55 +0530

    added the constructor for Student class

--- src/main/java/com/sarthak/task1/Student.java
+++ src/main/java/com/sarthak/task1/Student.java
@@ -10,4 +10,14 @@ public class Student {
     private String firstName;
     private String lastName;
     private byte currentYear;
+
+    public Student() {
+    }
+
+    public Student(byte rollNumber, String firstName, String lastName, byte currentYear) {
+        this.rollNumber = rollNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.currentYear = currentYear;
+    }
 }
