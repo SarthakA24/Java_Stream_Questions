0e653bc55c90ca66de12a1ca4f55265ecf80704d
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 18:50:47 +0530

    added the class EmployeeLeaveCalculator.java

---
+++ src/main/java/com/sarthak/task3/EmployeeLeaveCalculator.java
@@ -0,0 +1,9 @@
+/*
+ * Author Name: Sarthak Agarwal
+ * Date: 31/08/22
+ * Created With: IntelliJ IDEA Community Edition
+ */
+package com.sarthak.task3;
+
+public class EmployeeLeaveCalculator {
+}
