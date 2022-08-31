29944638d5ddda3dcaa75497d38ce1f4ae3d7f4c
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-08-31 18:53:50 +0530

    declared an object and called the methods

--- src/main/java/com/sarthak/task3/Main.java
+++ src/main/java/com/sarthak/task3/Main.java
@@ -7,6 +7,8 @@ package com.sarthak.task3;
 
 public class Main {
     public static void main(String[] args) {
-
+        EmployeeLeaveCalculator employeeLeaveCalculator = new EmployeeLeaveCalculator();
+        employeeLeaveCalculator.getDefaulterEmployeeIDList(null);
+        employeeLeaveCalculator.getNumberOfLeaves(null);
     }
 }
